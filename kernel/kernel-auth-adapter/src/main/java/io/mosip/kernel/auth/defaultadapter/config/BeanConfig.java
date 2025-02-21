d47cf45c5a081f46829bc40d6be0238e0503de7d
Ashok Kumar Sharma <ashok@mosip.io>
2025-02-22 01:38:24 +0530

    Added socket timeout for the regproc.

--- kernel/kernel-auth-adapter/src/main/java/io/mosip/kernel/auth/defaultadapter/config/BeanConfig.java
+++ kernel/kernel-auth-adapter/src/main/java/io/mosip/kernel/auth/defaultadapter/config/BeanConfig.java
@@ -13,10 +13,14 @@ import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
 
+import io.mosip.kernel.core.util.StringUtils;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -79,6 +83,8 @@ public class BeanConfig {
 
 	@Autowired(required = false)
 	private LoadBalancerClient loadBalancerClient;
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(BeanConfig.class);
 
 	@SuppressWarnings("java:S5527") // added suppress for sonarcloud. 
 	// Server hostname verification is not required because of 2 reasons:
@@ -151,6 +157,15 @@ public class BeanConfig {
 			});
 			httpClientBuilder.setSSLSocketFactory(csf);
 		}
+
+		//Setting socket timeout for the registration processor stages
+		String socketTimeout = environment.getProperty("registration.processor.socket.timeout");
+		if(StringUtils.isNotEmpty(socketTimeout)) {
+			LOGGER.info("Setting response timeout for the registration processor : {}", socketTimeout);
+			RequestConfig config = RequestConfig.custom().setSocketTimeout(Integer.parseInt(socketTimeout)).build();
+			httpClientBuilder.setDefaultRequestConfig(config);
+		}
+
 		String applName = getApplicationName();
 		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
 		requestFactory.setHttpClient(httpClientBuilder.build());
